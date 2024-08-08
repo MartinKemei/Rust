4fd21982a9ce819a9b0dcb99542cbb701513374d
MartinKemei <martinkemei254@gmail.com>
2024-08-08 11:13:23 +0300

    first line of rust code

---
+++ hello_world/target/debug/deps/hello_world-2304693e34e1dcdc.d
@@ -0,0 +1,5 @@
+/home/martin_kemei/Rust/hello_world/target/debug/deps/hello_world-2304693e34e1dcdc: src/main.rs
+
+/home/martin_kemei/Rust/hello_world/target/debug/deps/hello_world-2304693e34e1dcdc.d: src/main.rs
+
+src/main.rs:
---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/home/martin_kemei/Rust/hello_world/target/debug/hello_world: /home/martin_kemei/Rust/hello_world/src/main.rs
